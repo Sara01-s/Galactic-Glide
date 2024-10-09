82841fd570b3e263f790f546a765fd4d0bcf1984
Sara01 <saraosses01@gmail.com>
2024-10-09 11:52:24 -0300

    xd

---
+++ Assets/Scripts/Gameplay/PlayerData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data")]
+public class Data : ScriptableObject {
+
+    public float PlayeYPosition = 0.0f;
+    public int PlayerScore = 0;
+    
+}
